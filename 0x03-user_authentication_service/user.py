942522662ba3275a3f59371e65e9b8b25fe46b87
Dean Birnie <deanicdl@gmail.com>
2024-01-25 09:26:52 +0200

    Task 0 - Add User model for user authentication

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module contains a SQLAlchemy model for user authentication.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    This class creates a user model for a database table named users.
+    """
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(Integer, primary_key=True)
+    reset_token = Column(String(250), nullable=False)
+    

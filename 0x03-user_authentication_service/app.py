1669c718a3c0242801b1bfeed688f999ff449a3f
Dean Birnie <deanicdl@gmail.com>
2024-01-27 01:04:18 +0200

    Add reset_password endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,5 +62,17 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """update password endpoint"""
+    email = request.form.get('email')
+    try:
+        user = AUTH.create_session(email)
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")

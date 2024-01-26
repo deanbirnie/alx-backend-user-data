db1e1e344d7121afcc2c5da136e2f4dc2da7aaf2
Dean Birnie <deanicdl@gmail.com>
2024-01-27 00:19:42 +0200

    Fix indentation in auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,7 +18,7 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-        
+
     def register_user(self, email: str, password: str) -> User:
         """register user"""
         try:

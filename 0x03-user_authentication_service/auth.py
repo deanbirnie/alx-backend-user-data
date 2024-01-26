ec418d015d44bcff20468b57881d2cbdc70ec058
Dean Birnie <deanicdl@gmail.com>
2024-01-27 00:15:01 +0200

    Docstring

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,5 +5,6 @@ import bcrypt
 
 def _hash_password(password: str) -> bytes:
     """
+    returns byte value of hashed password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())

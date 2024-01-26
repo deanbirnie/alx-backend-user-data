7302586b0d660da7748968500d129e41f3478ac0
Dean Birnie <deanicdl@gmail.com>
2024-01-27 00:41:34 +0200

    Add valid_login method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,3 +26,17 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validation of user credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                pwb = password.encode('utf-8')
+                pwh = user.hashed_password
+                if bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+                return True
+        except NoResultFound:
+            return False
+        return False
